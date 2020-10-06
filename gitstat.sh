8f5dc39d810b00c573f51abd063d12d68277da1d
Artur <ap@arturpolak.com>
2020-10-07 00:13:50 +0200

    Update

--- gitstat.sh
+++ gitstat.sh
@@ -1,12 +1,5 @@
 #/bin/bash
-
-gitusername=apsid
-
-echo Gitpass?
-read gitpass
-echo Thx
-
 for f in `ls -d */`
 do
-    test -d "$f.git" &&  echo -e "\n$f" && git --git-dir=$f.git --work-tree=$f status -s -b
+    test -d "$f.git" &&  echo -e "\n$f" && git --git-dir=$f.git --work-tree=$f pull -q && git --git-dir=$f.git --work-tree=$f status -s -b
 done
