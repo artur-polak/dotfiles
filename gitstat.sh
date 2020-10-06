312d5303955ff951ced29bc5932a57fb5b0ef2cf
Artur <ap@arturpolak.com>
2020-10-07 00:03:08 +0200

    Added auto pull for git

--- gitstat.sh
+++ gitstat.sh
@@ -1,4 +1,11 @@
 #/bin/bash
+
+gitusername=apsid
+
+echo Gitpass?
+read gitpass
+echo Thx
+
 for f in `ls -d */`
 do
     test -d "$f.git" &&  echo -e "\n$f" && git --git-dir=$f.git --work-tree=$f status -s -b
